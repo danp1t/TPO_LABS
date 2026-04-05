a30f2fdd59df4a8de48f7e0ba04a7ece070fc0bd
Danil Putintsev <workdanilput@mail.ru>
2026-04-05 13:33:44 +0300

    Сделал параметризованные тесты

--- lab1/lab1/src/test/java/case1/CosTest.java
+++ lab1/lab1/src/test/java/case1/CosTest.java
@@ -1,96 +1,54 @@
 package case1;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static com.danp1t.case1.Cos.calculate;
-import static com.danp1t.case1.Cos.PI;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class CosTest {
-    double delta = 1e-5;
-    double deltaForBigNumber = 1e-2;
+class CosTest {
 
-    @Test
-    public void testTable() {
-        assertEquals(1, calculate(0.0), delta);
-        assertEquals(0.86603, calculate(PI/6), delta);
-        assertEquals(0.70711, calculate(PI / 4), delta);
-        assertEquals(0.5, calculate(PI / 3), delta);
-        assertEquals(0, calculate(PI / 2), delta);
-        assertEquals(-0.5, calculate(2*PI / 3), delta);
-        assertEquals(-0.70711, calculate(3*PI / 4), delta);
-        assertEquals(-0.86603, calculate(5*PI / 6), delta);
-        assertEquals(-1, calculate(PI), delta);
-        assertEquals(-0.86603, calculate(7*PI / 6), delta);
-        assertEquals(-0.70711, calculate(5*PI / 4), delta);
-        assertEquals(-0.5, calculate(4*PI / 3), delta);
-        assertEquals(0, calculate(3*PI / 2), delta);
-        assertEquals(0.5, calculate(5*PI / 3), delta);
-        assertEquals(0.70711, calculate(7*PI / 4), delta);
-        assertEquals(0.86603, calculate(11*PI / 6), delta);
-        assertEquals(1, calculate(2*PI), delta);
-    }
+    private static final double DELTA_STANDARD = 1e-5;
+    private static final double DELTA_BIG = 1e-2;
 
-    @Test
-    //cos(x) = cos(x + 2*pi)
-    public void testPeriod() {
-        assertEquals(calculate(2*PI), calculate(0.0), delta);
-        assertEquals(calculate(PI/6 + 2*PI), calculate(PI / 6), delta);
-        assertEquals(calculate(PI/4 - 2*PI), calculate(PI / 4), delta);
-        assertEquals(calculate(PI/3 + 10*PI), calculate(PI / 3), delta);
-        assertEquals(calculate(PI/2 - 20*PI), calculate(PI / 2), delta);
-    }
-
-    @Test
-    // cos(x) = cos(-x)
-    public void testEven() {
-        assertEquals(calculate(-0.0), calculate(0.0), delta);
-        assertEquals(calculate(-PI/6), calculate(PI/6), delta);
-        assertEquals(calculate(-PI / 4), calculate(PI / 4), delta);
-        assertEquals(calculate(-PI / 3), calculate(PI / 3), delta);
-        assertEquals(calculate(-PI / 2), calculate(PI / 2), delta);
-    }
-
-    @Test
-    public void testTooBig() {
-        assertEquals(-0.86603, calculate(19*PI/6), delta);
-        assertEquals(0.5, calculate(37*PI/3), delta);
-        assertEquals(-0.70711, calculate(85*PI/4), delta);
-        assertEquals(1, calculate(30000*PI/2), delta);
-        assertEquals(-0.5, calculate(37040*PI/3), delta);
-        assertEquals(0.70711, calculate(214103*PI/4), delta);
-        assertEquals(0.86603, calculate(416603*PI/6), delta);
-        assertEquals(1, calculate(857238*PI), delta);
-    }
-
-    @Test
-    public void testTooSmall() {
-        assertEquals(-0.86603, calculate(-19*PI/6), delta);
-        assertEquals(0.5, calculate(-37*PI/3), delta);
-        assertEquals(-0.70711, calculate(-85*PI/4), delta);
-        assertEquals(1, calculate(-30000*PI/2), delta);
-        assertEquals(-0.5, calculate(-37040*PI/3), delta);
-        assertEquals(0.70711, calculate(-214103*PI/4), delta);
-        assertEquals(0.86603, calculate(-416603*PI/6), delta);
-        assertEquals(1, calculate(-857238*PI), delta);
-    }
-
-    @Test
-    public void testCriticalPoint() {
-        assertEquals(1, calculate(Math.pow(10, 200)*PI), deltaForBigNumber);
-        assertEquals(1, calculate(-Math.pow(10, 200)*PI), deltaForBigNumber);
-        assertEquals(1, calculate(Math.pow(10, 308)), deltaForBigNumber);
-        assertEquals(1, calculate(-Math.pow(10, 308)), deltaForBigNumber);
-        assertThrows(ArithmeticException.class, () -> calculate(Math.pow(10, 309)*PI));
-        assertThrows(ArithmeticException.class, () -> calculate(-Math.pow(10, 309)*PI));
-        assertEquals(0, calculate(PI / 2), delta);
-        assertEquals(1, calculate(0.0), delta);
-        assertEquals(1, calculate(Math.pow(10, -326)), delta);
-        assertEquals(1, calculate(-Math.pow(10, -326)), delta);
-
+    @ParameterizedTest
+    @CsvFileSource(resources = "/testTable.csv", numLinesToSkip = 1)
+    void testTable(double x, double expected) {
+        assertEquals(expected, calculate(x), DELTA_STANDARD);
     }
 
 
+    @ParameterizedTest
+    @CsvFileSource(resources = "/testPeriod.csv", numLinesToSkip = 1)
+    void testPeriod(double x, double period) {
+        assertEquals(calculate(x), calculate(x + period), DELTA_STANDARD);
+    }
 
+    @ParameterizedTest
+    @CsvFileSource(resources = "/testEven.csv", numLinesToSkip = 1)
+    void testEven(double x) {
+        assertEquals(calculate(x), calculate(-x), DELTA_STANDARD);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/testTooBig.csv", numLinesToSkip = 1)
+    void testTooBig(double x, double expected) {
+        assertEquals(expected, calculate(x), DELTA_STANDARD);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/testTooSmall.csv", numLinesToSkip = 1)
+    void testTooSmall(double x, double expected) {
+        assertEquals(expected, calculate(x), DELTA_STANDARD);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/testCriticalPoints.csv", numLinesToSkip = 1)
+    void testCriticalPoints(double x, double expected, boolean expectException) {
+        if (expectException) {
+            assertThrows(ArithmeticException.class, () -> calculate(x));
+        } else {
+            assertEquals(expected, calculate(x), DELTA_BIG);
+        }
+    }
 }
