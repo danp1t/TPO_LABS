331cf1a91f6100d52eaa2bef34040a956343b19f
Danil Putintsev <workdanilput@mail.ru>
2026-04-18 15:04:09 +0300

    Реализованы логарифметические функции

--- lab2/lab2/src/main/java/com/danp1t/log_function/Log3.java
+++ lab2/lab2/src/main/java/com/danp1t/log_function/Log3.java
@@ -1,4 +1,7 @@
 package com.danp1t.log_function;
 
 public class Log3 {
+    public static double calculate(double x, double epsilon) {
+        return Ln.calculate(x, epsilon) / Ln.calculate(3, epsilon);
+    }
 }
--- lab2/lab2/src/main/java/com/danp1t/log_function/Log5.java
+++ lab2/lab2/src/main/java/com/danp1t/log_function/Log5.java
@@ -1,4 +1,7 @@
 package com.danp1t.log_function;
 
 public class Log5 {
+    public static double calculate(double x, double epsilon) {
+        return Ln.calculate(x, epsilon) / Ln.calculate(5, epsilon);
+    }
 }
